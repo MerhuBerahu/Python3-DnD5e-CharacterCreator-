ce0e4dd77f403ba1d3791a3f39144f8fbd3ca679
Kalidor <wmclaughlan@gmail.com>
2020-04-30 21:35:22 +0100

    updates rollforstats

--- RollforStats.py
+++ RollforStats.py
@@ -11,32 +11,36 @@ def roll6():
     number = random.randint(1,6)
     return number
 
-StartingStats = {'Strength': 0,'Dexterity': 0,'Constitution':0 ,'Intelligence':0 ,'Wisdom': 0, 'Charisma': 0}
+def StatsRoll():
+    StartingStats = {'Strength': 0,'Dexterity': 0,'Constitution':0 ,'Intelligence':0 ,'Wisdom': 0, 'Charisma': 0}
 
-num = 0
-for key,value  in StartingStats.items(): # print the keys of StartingStats
-    num = num + 1
-    print("\n",num,"- {} ".format(key))
-    count =  0  
-    rolls = []  # create the list of roll results
+    num = 0
+    for key,value  in StartingStats.items(): # print the keys of StartingStats
+        num = num + 1
+        print("\n",num,"- {} ".format(key))
+        count =  0  
+        rolls = []  # create the list of roll results
 
-    while count < 4:
-        rolls.append(roll6())
-        count += 1
-    rolls.sort() # sort rolls into ascending order
+        while count < 4:
+            rolls.append(roll6())
+            count += 1
+        rolls.sort() # sort rolls into ascending order
 
-    print("You rolled: ",rolls)
-    rolls.pop(0) # drop the first (lowest) number in rolls
+        print("You rolled: ",rolls)
+        rolls.pop(0) # drop the first (lowest) number in rolls
 
-    total = 0
-    for i in rolls: 
-        total = total + i
+        total = 0
+        for i in rolls: 
+            total = total + i
 
-    print("Your three highest rolls were {}, for a total of {}.".format(rolls,total))
-    #### STUCK HERE ###
-    #need to update the value of the dictionary key to the total variable
-    StartingStats[key] = total
+        print("Your three highest rolls were {}, for a total of {}.".format(rolls,total))
+        #### STUCK HERE ###
+        #need to update the value of the dictionary key to the total variable
+        StartingStats[key] = total
 
-print("\n\nYor stats are:\n")
-for key,value  in StartingStats.items(): # print the keys and values of StartingStats
-    print("{} = {}".format(key, value))
+    print("\n\nYor stats are:\n")
+    for key,value  in StartingStats.items(): # print the keys and values of StartingStats
+        print("{} = {}".format(key, value))
+
+#Testing
+StatsRoll()
